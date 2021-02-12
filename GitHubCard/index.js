eb0c774872138278695333f65743ffc8e0e855e8
luiza nacif <luizancoelho@gmail.com>
2021-02-11 16:03:54 -0800

    timed out, but done. Need to test code first

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,10 @@
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
+    https://api.github.com/users/padawan98
 */
-
+const axios = require('axios').default;
+axios.get('https://api.github.com/users/padawan98').then(response=>{console.log('Step 1, data fetching: ',  response)}).catch(err => {console.log(err)});
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,6 +50,115 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker(myGithub)
+{
+  //class card
+  let newClass = document.createElement('div');
+  newClass.classList.add('card');
+
+  //img
+  let myImg = document.createElement('img'); //create img
+  myImg.setAttribute('src', myGithub.avatar_url); // putting img
+  newClass.appendChild(myImg);
+
+  //child class card-info
+  let cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  newClass.appendChild(cardInfo);
+
+  //h3
+  let h3 = document.createElement('h3');
+  h3.classList.add('name');
+  h3.textContent = myGithub.name;
+  cardInfo.appendChild(h3);
+
+  //username p 
+  let userName = document.createElement('p');
+  userName.classList.add('username');
+  userName.textContent = myGithub.login;
+  cardInfo.appendChild(userName);
+
+  //location p
+  let location = document.createElement('p');
+  location.textContent = 'Location: ' + myGithub.location;
+  cardInfo.appendChild(location);
+
+  //profile p 
+  let profile = document.createElement('p');
+  profile.textContent = 'Profile: ';
+  cardInfo.appendChild(profile);
+
+  //profile p a
+  let profileAnchor = document.createElement('a');
+  profileAnchor.href = 'https://api.github.com/users/' + myGithub.userName;
+  profileAnchor.textContent = 'https://api.github.com/users/' + myGithub.userName;
+  profile.appendChild(profileAnchor);
+
+  //profile followers
+  let followers = document.createElement('p');
+  followers.textContent = 'Followers: ' + myGithub.followers;
+  cardInfo.appendChild(followers);
+
+  //profile following
+  let following = document.createElement('p');
+  following.textContent = 'Following: ' + myGithub.following;
+  cardInfo.appendChild(following);
+
+  //bio
+  let bio = document.createElement('p');
+  bio.textContent = 'Bio: ' + myGithub.bio;
+  cardInfo.appendChild(bio);
+
+  //return
+  return newClass;
+}
+
+//achor to .cards
+let entryPoint = document.querySelector('.cards'); 
+ 
+//axios my profile
+axios.get('https://api.github.com/users/padawan98')
+.then(response =>
+  {
+    const myData = response.data;  //fetch data => myData
+    // console.log('Step 4 res = ', response);
+    // console.log('Step 4 res.data = ', response.data);
+    entryPoint.appendChild(cardMaker(myData)); //entryPoint = place in dom
+  })
+.catch(err =>
+  {
+    console.log(err);
+  });
+
+//putting following usernames in array
+axios.get('https://api.github.com/users/padawan98/following')
+.then(response =>
+  {
+    let myArr = response.data;
+    myArr.forEach(element => 
+    {
+      followersArray.push(element.login);  
+    });
+  })
+.catch(err =>
+  {
+    console.log(err);
+  });
+
+//looping for loop, fetching profile of following users
+followersArray.forEach(element =>
+{
+  axios.get('https://api.github.com/users/' + element)
+  .then(response =>
+    {
+      let frensData = response.data;  //fetch data => frensData
+      entryPoint.appendChild(cardMaker(frensData)); //entryPoint = place in dom
+    })
+  .catch(err =>
+    {
+      console.log(err);
+    }); 
+});
 
 /*
   List of LS Instructors Github username's:
